9fe24a4a7c7b7b0cc095d92154a1b178b5ecce6e
rsurfings <ronaldo.goncalves@signativa.com.br>
2023-07-11 15:44:58 -0300

    Implementation with version control and Spotify Dockerfile integration

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:17-jdk-alpine
-VOLUME /tmp
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
+MAINTAINER Ronaldo Goncalves <rgooncallves@gmail.com>
+ENTRYPOINT ["java", "-jar", "/usr/coopvote-api.jar"]
+ARG JAR_FILE
+ADD target/${JAR_FILE} /usr/coopvote-api.jar
 EXPOSE 8080
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- src/main/java/com/ronaldogoncalves/coopvote/configuration/OpenApiConfig.java
+++ src/main/java/com/ronaldogoncalves/coopvote/configuration/OpenApiConfig.java
@@ -1,6 +1,8 @@
 package com.ronaldogoncalves.coopvote.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,8 +10,16 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI usersMicroserviceOpenAPI() {
-        return new OpenAPI();
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo("1.0"));
+    }
+
+    private Info apiInfo(String version) {
+        return new Info().title("Coopvote assembly's, voting")
+                .description("Coopvote assembly's, voting")
+                .version(version);
     }
 
 }
