9bacb5da5ee88640599f9a14c4c64333cdea3377
rsurfings <ronaldo.goncalves@signativa.com.br>
2023-07-12 15:41:41 -0300

    Implementation fix test contextLoads fail

--- src/test/java/com/ronaldogoncalves/coopvote/CoopvoteApplicationTests.java
+++ src/test/java/com/ronaldogoncalves/coopvote/CoopvoteApplicationTests.java
@@ -1,8 +1,12 @@
 package com.ronaldogoncalves.coopvote;
 
+import com.ronaldogoncalves.coopvote.configuration.KafkaConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
+@ContextConfiguration(classes = KafkaConfiguration.class, loader = AnnotationConfigContextLoader.class)
 @SpringBootTest
 class CoopvoteApplicationTests {
 
